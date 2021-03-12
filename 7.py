9befb604eb761bf6be0260666f5f97d2ca7552ad
Ankit Patil <80503391+ankitpatil30@users.noreply.github.com>
2021-03-12 01:44:30 -0500

    Task 1

---
+++ 7.py
@@ -0,0 +1,13 @@
+#TASK ONE NUMBERS AND VARIABLES
+#7. Create Variables using formats such as Upper CamelCase, Lower CamelCase, SnakeCase and
+#UPPERCASE.
+#(Refer: https://capitalizemytitle.com/camel-case/)
+
+#Upper CamelCase:
+MyVariable = 10
+#Lower CamelCase:
+myVariable = 20
+#SnakeCase:
+my_variable = 30
+#UPPERCASE:
+MYVARIABLE = 40
