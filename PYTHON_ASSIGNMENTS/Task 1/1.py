d56644eb229f6d8bc2f7847ffe8928123ec966f3
Ankit Patil <80503391+ankitpatil30@users.noreply.github.com>
2021-03-12 01:56:24 -0500

    Delete PYTHON_ASSIGNMENTS/Task 1 directory

--- PYTHON_ASSIGNMENTS/Task 1/1.py
+++
@@ -1,5 +0,0 @@
-#TASK ONE NUMBERS AND VARIABLES
-#1. Create three variables in a single line and assign values to them in such a manner that each one of them belongs to a different data type.
-
-a, b, c = 5, 5.55, 'SSSS'
-print(type(a), type(b), type(c))
