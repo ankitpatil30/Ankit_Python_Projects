198524c15fd29f07bd0792c95b8791a8a8bf2800
Ankit Patil <80503391+ankitpatil30@users.noreply.github.com>
2021-03-12 02:00:45 -0500

    Task 1

---
+++ Python_Assignments/Task 1/1.py
@@ -0,0 +1,5 @@
+#TASK ONE NUMBERS AND VARIABLES
+#1. Create three variables in a single line and assign values to them in such a manner that each one of them belongs to a different data type.
+
+a, b, c = 5, 5.55, 'SSSS'
+print(type(a), type(b), type(c))
---
+++ Python_Assignments/Task 1/4.py
@@ -0,0 +1,7 @@
+#TASK ONE NUMBERS AND VARIABLES
+#4. Write a program that takes input from the user and prints it using both Python 2.x and Python 3.x Version.
+
+color = raw_input("Enter the color name: ")
+#print(color)
+color = input("Enter the colour name: ")
+print(color)
