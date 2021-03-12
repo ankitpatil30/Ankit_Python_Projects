24483463a113e8f98d918fe5fc69c65cea1eca7d
Ankit Patil <80503391+ankitpatil30@users.noreply.github.com>
2021-03-12 02:01:39 -0500

    Add files via upload

---
+++ Python_Assignments/Task 1/8.py
@@ -0,0 +1,10 @@
+#TASK ONE NUMBERS AND VARIABLES
+#8. If one data type value is assigned to ‘a’ variable and then a different data type value is assigned to ‘a’
+#again. Will it change the value? If Yes then Why?
+
+a = 5
+print(a)
+a = 5.5
+print(a)
+
+# Yes, because in python you can assign the values dynamically to the same variable
