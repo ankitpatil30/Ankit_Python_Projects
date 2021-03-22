402a9fccd031b86c670a9b8e30e6d4080722c45c
Ankit Patil <80503391+ankitpatil30@users.noreply.github.com>
2021-03-22 00:09:54 -0400

    Task 4

---
+++ Python_Assignments/Task 4/5.py
@@ -0,0 +1,11 @@
+# TASK FOUR
+# TRADITIONAL FUNCTIONS,ANONYMOUS FUNCTIONS &
+# HIGHER ORDER FUNCTIONS
+# 5. Write a program that accepts a sequence of lines as input and prints the lines after making all
+# characters in the sentence capitalized.
+# Sample input: Hello world Practice makes man perfect
+# Expected output: HELLO WORLD PRACTICE MAKES MAN PERFECT
+
+x = input("Entera sequence of lines: ")
+z = x.upper()
+print(z)
