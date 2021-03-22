742c221653fde3670cac1716b40165a8a8ff3d5d
Ankit Patil <80503391+ankitpatil30@users.noreply.github.com>
2021-03-22 00:11:56 -0400

    Task 4

---
+++ Python_Assignments/Task 4/6.py
@@ -0,0 +1,12 @@
+# TASK FOUR
+# TRADITIONAL FUNCTIONS,ANONYMOUS FUNCTIONS &
+# HIGHER ORDER FUNCTIONS
+# 6. Define a function that can receive two integral numbers in string form and compute their sum and
+# print it in the console.
+
+def add(q, w):
+    sum = int(q) + int(w)
+    return sum
+x = "10"
+y = "20"
+print(add(x, y))
