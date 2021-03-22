0d76e1177395acc0a626c1e8c8ad0b7d65636b81
Ankit Patil <80503391+ankitpatil30@users.noreply.github.com>
2021-03-22 00:07:45 -0400

    Delete 4.py

--- Python_Assignments/4.py
+++
@@ -1,9 +0,0 @@
-# TASK FOUR
-# TRADITIONAL FUNCTIONS,ANONYMOUS FUNCTIONS &
-# HIGHER ORDER FUNCTIONS
-# 4. Write a program that accepts a hyphen-separated sequence of words as input and prints the words
-# in a hyphen-separated sequence after sorting them alphabetically.
-
-items=[n for n in input("Enter the hyphen-seperated string sequence: ").split('-')]
-items.sort()
-print("Sorted Sequence: ", '-'.join(items))
