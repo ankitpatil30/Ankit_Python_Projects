785629364612acfd226dd76b2d48292565470c95
Ankit Patil <80503391+ankitpatil30@users.noreply.github.com>
2021-03-24 19:03:24 -0400

    Delete 2.py

--- Python_Assignments/Additional Task/2.py
+++
@@ -1,6 +0,0 @@
-# EXTRA TASK
-# DATA STRUCTURES
-# 2. Create a list of thousand numbers using range and xrange and see the difference between each
-# other.
-# (For reference:https://www.techbeamers.com/python-xrange-range/)
-
