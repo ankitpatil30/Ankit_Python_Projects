1a9570013ff17aa28d9aa97167ec8fbc990c6f68
Ankit Patil <80503391+ankitpatil30@users.noreply.github.com>
2021-03-24 19:03:43 -0400

    Additional Task

---
+++ Python_Assignments/Additional Task/2.py
@@ -0,0 +1,12 @@
+# EXTRA TASK
+# DATA STRUCTURES
+# 2. Create a list of thousand numbers using range and xrange and see the difference between each
+# other.
+# (For reference:https://www.techbeamers.com/python-xrange-range/)
+
+x = [i for i in range(1000)]
+print(x)
+
+# x = [i for i in xrange(1000)]
+# print(x)
+# xrange() is for python 2.x
